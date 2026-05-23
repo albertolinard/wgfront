97fdfe205defb058f5de3c19f947213c33dfee65
Alberto Linard <albertolinard@gmail.com>
2026-05-23 19:13:45 -0300

    chore: update site URL for self-hosted deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wireguard.otaviomiranda.com.br',
+  site: 'https://wireguard.albertolinard.com',
 });
